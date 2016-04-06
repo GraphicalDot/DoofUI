fdd00bdf2d13bb002f3842da0815a56cb8dd8291
Rahul Garg <rahul_garg08@yahoo.com>
2016-04-06 17:03:57 +0530

    Changes which are gonna change doof's life.

--- app/js/doof.js
+++ app/js/doof.js
@@ -4,38 +4,37 @@ define(function (require) {
 	var Backbone = require('backbone');
 	var Marionette = require('marionette');
 
+	var LoaderView= require('./loader/i-loader');
 	var FacebookUser = require('./models/facebookUser');
 	var Router = require('./router');
-	// var GetKeyModel= require('./models/getkey');
-	// var ApisModel = require('./models/apis');
 
 	var Doof = new Marionette.Application();
 	Doof.addRegions({ region: '.doof' })
 
 	Doof.on("before:start", function () {
-		// find token here. Going to be done later.
-		// var key= new GetKeyModel();
-		// key.fetch({data: { secret: "967d2b1f6111a198431532149879983a1ad3501224fb0dbf947499b1" }, type: 'POST'});
-		// var apis = new ApisModel();
-		// var pkey = apis.get('privateKey');
-		// apis.fetch({ data: { "key": pkey }, type: 'POST' });
+		// Check For Facebook Login Status.
+		var fbUser = new FacebookUser();
+		fbUser.init()
+		.then(function () { return fbUser.checkLogin(); })
+		.then(function () { return fbUser.fetchInfo(); })
+		.then(function () {
+			fbUser.sendUserData();
+			Doof.vent.trigger("start:app", fbUser);
+		})
+		.catch(console.log.bind(console));
 	});
 
-	Doof.on("start", function () {
+	Doof.on("start", function (fbUser) {
+		Doof.router = new Router({ region: Doof.region, user: fbUser });
+		if (Backbone.history) {
+			Backbone.history.start();
+		}
 		//wait for facebook to hide loading and continue.
-		var fbUser = new FacebookUser();
-		fbUser.init().then(function () {
-			return fbUser.checkLogin();
-		}).then(function () {
-			return fbUser.fetchInfo();
-		}).then(function () {
-			fbUser.sendUserData();
-			Doof.router = new Router({ region: Doof.region, user: fbUser });
-			if (Backbone.history) {
-				Backbone.history.start();
-			}
-		}).catch(console.log.bind(console));
 	});
 
 	return Doof;
 });
+
+// Pending
+// var key= new GetKeyModel(); key.fetch({data: { secret: "967d2b1f6111a198431532149879983a1ad3501224fb0dbf947499b1" }, type: 'POST'});
+// var apis = new ApisModel(); var pkey = apis.get('privateKey'); apis.fetch({ data: { "key": pkey }, type: 'POST' });

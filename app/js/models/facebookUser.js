e4711d2fa2f0820e6ae4069d4733583edd2e8c2d
Rahul Garg <rahul_garg08@yahoo.com>
2016-04-07 14:08:59 +0530

    Reffactored facebook user model.

--- app/js/models/facebookUser.js
+++ app/js/models/facebookUser.js
@@ -1,14 +1,14 @@
 define(function (require) {
 	"use strict";
 
+	var $ = require('jquery');
 	var Backbone = require('backbone');
-	var Promise = require('es6promise').Promise;
 	var FB = require('facebook');
-
-	var UserDetailModel = require('./userdetails');
+	var Promise = require('es6promise').Promise;
 
 	var UserModel = Backbone.Model.extend({
 		defaults: { id: null, third_party_id: null, name: null, email: null, image: null, status: 0 },
+		// Initialize FB SDK
 		init: function () {
 			var promise = new Promise(function (resolve) {
 				FB.init({
@@ -21,6 +21,7 @@ define(function (require) {
 			});
 			return promise;
 		},
+		// Check if User is Logged In using FB method. Update status accordingly.
 		checkLogin: function () {
 			var self = this;
 			var promise = new Promise(function (resolve) {
@@ -33,6 +34,7 @@ define(function (require) {
 			});
 			return promise;
 		},
+		// Fetch userinfo if user is logged in.. otherwise do nothing..
 		fetchInfo: function () {
 			var self = this;
 			var promise = new Promise(function (resolve, reject) {
@@ -56,15 +58,28 @@ define(function (require) {
 			});
 			return promise;
 		},
+		// Send User Data for analytics.
 		sendUserData: function () {
 			if (this.get('status')) {
-				var userDetails = new UserDetailModel();
-				userDetails.fetch({ method: 'POST', data: { email: this.get('email'), fb_id: this.get('id'), picture: this.get('picture'), name: this.get('name') } });
+				$.ajax({
+					url: "http://52.76.176.188:8000/usersdetails",
+					dataType: 'json',
+					method: 'POST',
+					data: { email: this.get('email'), fb_id: this.get('id'), picture: this.get('picture'), name: this.get('name') },
+					success: function () {
+						console.log('current user information successfully sent');
+					},
+					error: function () {
+						console.log('userdetails api not working..');
+					}
+				});
 			}
 		},
+		// This method checks if current user is authorized [return true, false]
 		isAuthorized: function () {
 			return Boolean(this.get("third_party_id"));
 		},
+		// Facebook method for logging in
 		login: function () {
 			var self = this;
 			var promise = new Promise(function (resolve, reject) {
@@ -84,8 +99,9 @@ define(function (require) {
 			});
 			return promise;
 		},
+		// Facebook method for logging out
 		logout: function () {
-			var self= this;
+			var self = this;
 			var promise = new Promise(function (resolve) {
 				FB.logout(function () {
 					self.clear();

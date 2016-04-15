636119bf55ec674bb8a855daf8ff4b3f9994d087
Rahul Garg <rahul_garg08@yahoo.com>
2016-04-15 13:10:06 +0530

    Added comment to application method

--- app/js/router.js
+++ app/js/router.js
@@ -36,13 +36,19 @@ define(function (require) {
 		landingPage: function () {
 			var self = this;
 			var landingPage = new LandingPage({ user: this.user, dataService: this.dataService, googleService: this.googleService });
-			landingPage.on('goToApplication', function (position, trending_eateries) {
-				self.application(position, trending_eateries);
+			landingPage.on('goToApplication', function (position, eateries, eateryCategory) {
+				self.application(position, eateries, eateryCategory);
 			});
 			this.region.show(landingPage);
 		},
-		application: function (position, nearestEateriesList) {
-			var applicationPage = new ApplicationPage({ user: this.user, position: position, eateries: nearestEateriesList, dataService: this.dataService, googleService: this.googleService });
+		/**
+		 * Loads Application View.
+		 * @params position -> {latLng: {lat: startingLat, lng: startingLng}, place: 'New Delhi'}
+		 * @params eateries -> List of eateries to show [Optional]
+		 * @params eateryCategory -> Trending/ nearby [Type of eateries in one params ]
+		 */
+		application: function (position, eateries, eateryCategory) {
+			var applicationPage = new ApplicationPage({ user: this.user, position: position, eateries: eateries, category: eateryCategory, dataService: this.dataService, googleService: this.googleService });
 			this.region.show(applicationPage);
 		}
 	});
